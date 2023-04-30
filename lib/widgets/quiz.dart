4a19dd3efe66b07a9314496b06d0dc92aee2a2e7
johnma <ma.jonathan02@gmail.com>
2023-04-30 05:51:11 -0400

    style: refactor and playing with Problem widget

--- lib/widgets/quiz.dart
+++ lib/widgets/quiz.dart
@@ -37,25 +37,32 @@ class _QuizState extends State<Quiz> {
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            width: 350,
-            height: 150,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.amber[50],
-            ),
-            child: Center(child: Text(problems108[indices[index]].problem)),
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Container(
+          width: 350,
+          height: 150,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.amber[50],
           ),
-          const SizedBox(height: 30),
-          ...problems108[indices[index]].options.asMap().entries.map(
-                (x) => CustomRadioButton(
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(problems108[indices[index]].problem),
+          ),
+        ),
+        const SizedBox(height: 30),
+        ...problems108[indices[index]].options.asMap().entries.map(
+              (x) => Column(children: [
+                CustomRadioButton(
                     text: x.value,
                     selectedOption: widget.selectedOption,
                     onSelectOption: widget.onSelectOption,
                     option: x.key),
-              ),
-        ]);
+                const SizedBox(height: 10)
+              ]),
+            ),
+      ],
+    );
   }
 }
