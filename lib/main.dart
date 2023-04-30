d937e801c32c5e665b5248f5650567e09b5f92ed
janahmedprg <jan.ahmed.prg@gmail.com>
2023-04-29 22:34:14 -0400

    corgi spawns minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,8 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
@@ -102,7 +103,14 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                       child: XpBar(exp: _exp),
                     ))
                   ])
-                : SharePage(exp: _exp, incrementExp: _incrementExp)),
+                : Column(children: <Widget>[
+                    SharePage(exp: _exp, incrementExp: _incrementExp),
+                    Expanded(
+                        child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: XpBar(exp: _exp),
+                    ))
+                  ])),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color.fromARGB(255, 218, 225, 233),
